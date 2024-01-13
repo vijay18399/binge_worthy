97a911e2b89315ccbe172962a4cb7f8fe1dc4979
vijay18399 <vijayreddy18399@gmail.com>
2024-01-13 09:49:05 +0530

    v3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HAMMER_GESTURE_CONFIG, HammerGestureConfig, HammerModule } from '@angular/platform-browser';
 import * as Hammer from 'hammerjs';
 
 export class MyHammerConfig extends HammerGestureConfig {
   override overrides = <any>{
-  swipe: { direction: Hammer.DIRECTION_ALL },
+    swipe: { direction: Hammer.DIRECTION_ALL },
   };
 }
 @NgModule({
@@ -24,7 +24,15 @@ export class MyHammerConfig extends HammerGestureConfig {
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [{ provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig }],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig
+    },
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
